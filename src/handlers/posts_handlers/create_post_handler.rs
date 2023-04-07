5b5c1ab07a595714f5b5d97c33bec35936ecd477
Winner13KS <kuandyksanzar020@gmail.com>
2023-04-07 14:06:57 +0600

    init post handler

---
+++ src/handlers/posts_handlers/create_post_handler.rs
@@ -0,0 +1,15 @@
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Extension, Json,
+};
+use bson::{doc, oid::ObjectId};
+use mongodb::Client;
+
+pub async fn create_post(
+    State(client): State<Client>,
+    Extension(user_id): Extension<ObjectId>,
+    Path(channel_id): Path<String>,
+) -> impl IntoResponse {
+}
