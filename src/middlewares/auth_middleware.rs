9272a450c2a89b4530224505e5a15235366f2c5d
Winner13KS <kuandyksanzar020@gmail.com>
2023-04-08 14:05:26 +0600

    imrpoved auth_middleware

--- src/middlewares/auth_middleware.rs
+++ src/middlewares/auth_middleware.rs
@@ -13,37 +13,29 @@ pub async fn auth<B>(
     mut req: Request<B>,
     next: Next<B>,
 ) -> Result<Response, StatusCode> {
-    let auth_header = req
-        .headers()
-        .get(http::header::AUTHORIZATION)
-        .and_then(|header| header.to_str().ok());
-
-    let token = if let Some(token) = auth_header {
-        token
-    } else {
-        return Err(StatusCode::UNAUTHORIZED);
+    let auth_header = match req.headers().get(http::header::AUTHORIZATION) {
+        Some(header) => header.to_str().ok(),
+        None => None,
     };
 
-    let jwt_secret = std::env::var("JWT_SECRET");
+    let jwt_secret = std::env::var("JWT_SECRET").map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    if let Ok(token_info) = verify_token(token, &jwt_secret.unwrap()) {
-        let user_id = match ObjectId::parse_str(&token_info.sub) {
-            Ok(id) => id,
-            Err(_) => return Err(StatusCode::UNAUTHORIZED),
-        };
+    let token = match auth_header {
+        Some(token) => token,
+        None => return Err(StatusCode::UNAUTHORIZED),
+    };
 
-        //access collection to check user_id for existence in user table
-        let collection: Collection<User> = client.database("Merume").collection("users");
+    let token_claims = verify_token(token, &jwt_secret).map_err(|_| StatusCode::UNAUTHORIZED)?;
 
-        if let Ok(Some(_)) = collection.find_one(doc! {"_id": user_id}, None).await {
-            // Document with the specified ObjectId exists in the collection
-            req.extensions_mut().insert(user_id);
-            return Ok(next.run(req).await);
-        } else {
-            // Document with the specified ObjectId does not exist in the collection
-            return Err(StatusCode::UNAUTHORIZED);
-        }
-    } else {
-        return Err(StatusCode::UNAUTHORIZED);
-    }
+    let user_id = ObjectId::parse_str(&token_claims.sub).map_err(|_| StatusCode::UNAUTHORIZED)?;
+
+    let collection: Collection<User> = client.database("Merume").collection("users");
+    match collection.find_one(doc! {"_id": user_id}, None).await {
+        Ok(Some(user)) => user,
+        Ok(None) => return Err(StatusCode::UNAUTHORIZED),
+        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+    };
+
+    req.extensions_mut().insert(user_id);
+    Ok(next.run(req).await)
 }
