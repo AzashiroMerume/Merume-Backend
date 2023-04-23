fd758a48f3b7b74f93fbb224d4e71e34001c07b9
Winner13KS <kuandyksanzar020@gmail.com>
2023-04-23 19:59:10 +0600

    base recomendations

--- src/handlers/content_recomendation_handlers/recomendations_handler.rs
+++ src/handlers/content_recomendation_handlers/recomendations_handler.rs
@@ -2,10 +2,13 @@ use axum::{extract::State, http::StatusCode, response::IntoResponse, Extension, 
 use bson::{doc, oid::ObjectId};
 use mongodb::{options::UpdateOptions, Client};
 
-use crate::models::user_model::User;
+use crate::models::{user_model::User, post_model::Post};
 
 pub async fn recomendations(
     State(client): State<Client>,
     Extension(user): Extension<User>,
 ) -> impl IntoResponse {
+    let user_preferences = user.preferences.unwrap();
+
+    let post_collection = client.database("Merume").collection::<Post>("posts");
 }
