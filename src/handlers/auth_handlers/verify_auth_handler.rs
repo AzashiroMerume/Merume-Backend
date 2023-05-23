3351173072c8e79754e4f79d3fdd983a1ad7aa80
Winner13KS <kuandyksanzar020@gmail.com>
2023-05-23 16:36:43 +0600

    check for user preferences

--- src/handlers/auth_handlers/verify_auth_handler.rs
+++ src/handlers/auth_handlers/verify_auth_handler.rs
@@ -1,5 +1,25 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
 
-pub async fn verify_auth() -> impl IntoResponse {
-    StatusCode::OK
+use crate::{models::user_model::User, responses::AuthResponse};
+
+pub async fn verify_auth(Extension(user): Extension<User>) -> impl IntoResponse {
+    if user.preferences.is_none() {
+        (
+            StatusCode::OK,
+            Json(AuthResponse {
+                success: false,
+                token: None,
+                error_message: Some("User does not have preferences".to_string()),
+            }),
+        )
+    } else {
+        (
+            StatusCode::OK,
+            Json(AuthResponse {
+                success: true,
+                token: None,
+                error_message: None,
+            }),
+        )
+    }
 }
--- src/routes/auth_routes.rs
+++ src/routes/auth_routes.rs
@@ -14,7 +14,7 @@ pub fn auth_routes(State(state): State<AppState>) -> Router<AppState> {
         .route("/", get(auth_handlers::verify_auth_handler::verify_auth))
         .route("/logout", post(auth_handlers::logout_handler::logout))
         .route_layer(middleware::from_fn_with_state(state, |state, req, next| {
-            auth_middleware::auth(state, req, next, Some(false))
+            auth_middleware::auth(state, req, next, Some(true))
         }))
         .route("/register", post(auth_handlers::register_handler::register))
         .route("/login", post(auth_handlers::login_handler::login))
