d212788255dc73d01db84f868b3b38e15696991d
AzashiroMerume <kuandyksanzar020@gmail.com>
2023-10-08 17:26:23 +0600

    Revert "Revert "id  is assigned from payload, not generated""
    This reverts commit 4cdd53fbb14b0ac59ba644a308ab0ef6c9e1d2ec.

--- src/handlers/channels_handlers/get_channel_posts_handler.rs
+++ src/handlers/channels_handlers/get_channel_posts_handler.rs
@@ -9,9 +9,10 @@ use axum::{
 };
 use bson::{doc, oid::ObjectId};
 use futures::{SinkExt, StreamExt, TryStreamExt};
-// use mongodb::options::{ChangeStreamOptions, FullDocumentType};
-
-use serde_json::json;
+use mongodb::{
+    change_stream::event::ChangeStreamEvent,
+    options::{ChangeStreamOptions, FindOptions, FullDocumentType},
+};
 
 pub async fn channel_posts(
     ws: WebSocketUpgrade,
@@ -31,51 +32,31 @@ async fn websocket(
     let (mut sender, _receiver) = _socket.split();
 
     // Retrieve initial posts
-    let initial_posts = fetch_initial_posts(state.clone(), channel_id).await;
+    let initial_posts = fetch_posts(state.clone(), channel_id).await;
 
-    for post in initial_posts {
-        let json = json!(post);
-
-        if let Ok(json_str) = serde_json::to_string(&json) {
-            if let Err(err) = sender.send(Message::Text(json_str)).await {
-                eprintln!("Error sending message to websocket client: {:?}", err);
-                return;
-            }
-        } else {
-            eprintln!("Error serializing post to JSON");
+    if let Ok(json) = serde_json::to_string(&initial_posts) {
+        if let Err(err) = sender.send(Message::Text(json)).await {
+            eprintln!("Error sending message to websocket client: {:?}", err);
             return;
         }
-    }
-
-    let is_subscribed = is_user_subscribed(user_id, channel_id, &state).await;
-    let is_public = is_channel_public(channel_id, &state).await;
-
-    if !is_subscribed && !is_public {
-        // User is not subscribed and channel is not public, close the websocket
+    } else {
+        eprintln!("Error serializing posts to JSON");
         return;
     }
 
-    // let pipeline = vec![doc! {
-    //     "$match": {
-    //         "channel_id": channel_id
-    //     }
-    // }];
+    // if !is_channel_public(channel_id, &state).await
+    //     && !is_user_subscribed(user_id, channel_id, &state).await
+    // {
+    //     return;
+    // }
 
-    // let options = ChangeStreamOptions::builder()
-    //     .full_document(Some(FullDocumentType::UpdateLookup))
-    //     .build();
-
-    let pipeline = vec![doc! {
-        "$match": {
-            "channel_id": channel_id
-        }
-    }];
+    let pipeline = [doc! {"$match": {"channel_id": channel_id}}];
 
     // Listen for changes in channel posts
     let change_stream = state
         .db
         .posts_collection
-        .watch(pipeline, None)
+        .watch(None, None)
         .await
         .map_err(|err| {
             eprintln!("Error creating change stream: {:?}", err);
@@ -83,42 +64,51 @@ async fn websocket(
         });
 
     if let Ok(mut change_stream) = change_stream {
-        while let Some(change_event) = change_stream.next().await {
-            if let Ok(change_event) = change_event {
-                if let Some(post) = change_event.full_document {
-                    if post.channel_id == channel_id {
-                        let json = json!(post);
+        println!("Changed");
+        loop {
+            match change_stream.try_next().await {
+                Ok(Some(_)) => {
+                    let posts = fetch_posts(state.clone(), channel_id).await;
 
-                        if let Ok(json_str) = serde_json::to_string(&json) {
-                            if let Err(err) = sender.send(Message::Text(json_str)).await {
+                    if let Some(posts) = posts {
+                        let json = serde_json::to_string(&posts).map_err(|err| {
+                            eprintln!("Error serializing posts: {:?}", err);
+                            "Failed to serialize posts".to_string()
+                        });
+
+                        if let Ok(json) = json {
+                            if let Err(err) = sender.send(Message::Text(json)).await {
                                 eprintln!("Error sending message to websocket client: {:?}", err);
                                 break;
                             }
                         } else {
-                            eprintln!("Error serializing post to JSON");
                             break;
                         }
                     }
                 }
-            } else {
-                eprintln!("Error reading change stream");
-                break;
+                Ok(None) => {
+                    break;
+                }
+                Err(err) => {
+                    eprintln!("Error reading change stream: {:?}", err);
+                    break;
+                }
             }
         }
     }
 }
 
-async fn fetch_initial_posts(state: State<AppState>, channel_id: ObjectId) -> Vec<Post> {
+async fn fetch_posts(state: State<AppState>, channel_id: ObjectId) -> Option<Vec<Post>> {
     let filter = doc! {"channel_id": channel_id};
 
-    let options = mongodb::options::FindOptions::builder()
+    let options = FindOptions::builder()
         .sort(doc! {"timestamp": -1})
         .limit(20)
         .build();
 
     if let Ok(cursor) = state.db.posts_collection.find(filter, options).await {
         if let Ok(posts) = cursor.try_collect().await {
-            return posts;
+            return Some(posts);
         } else {
             eprintln!("Error collecting posts");
         }
@@ -126,7 +116,7 @@ async fn fetch_initial_posts(state: State<AppState>, channel_id: ObjectId) -> Ve
         eprintln!("Error finding posts");
     }
 
-    vec![]
+    None
 }
 
 async fn is_user_subscribed(user_id: ObjectId, channel_id: ObjectId, state: &AppState) -> bool {
