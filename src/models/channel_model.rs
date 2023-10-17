abe5b38a268b6b6df7489bdef6f1875c2d34e7e1
AzashiroMerume <kuandyksanzar020@gmail.com>
2023-10-17 13:32:26 +0600

    basic participants init

--- src/models/channel_model.rs
+++ src/models/channel_model.rs
@@ -16,6 +16,7 @@ pub struct Channel {
     pub channel_type: String,
     pub description: String,
     pub categories: Vec<String>,
+    pub participants: Option<Vec<ObjectId>>,
     pub followers: Followers,
     pub current_challenge_day: usize,
     #[serde(skip_serializing_if = "Option::is_none")]
